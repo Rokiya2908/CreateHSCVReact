75be90f5534cb0a9d7206942f3ec6f45b667d14c
Rokiya <khangvm2908@gmail.com>
2026-06-19 16:24:21 +0700

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
 import './App.css'
 
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import EmoplyeeList from './pages/EmployeeList'
 import MainPage from './pages/MainPage'
 
 function App() {
   // const [count, setCount] = useState(0)
   return (
-    <div>
-    <a href='/EmployeeList'>Employ Page</a>
-    <a href='/MainPage'> Main Page
-    </a>
-    
-    <BrowserRouter>
-      <Routes>
+    <>
+      <div>
+        <a href='/EmployeeList'>Employ Page</a>
+        <a href='/MainPage'> Main Page
+        </a>
+      </div>
+
+      <BrowserRouter>
+        <Routes>
           <Route
-            path ="/EmployeeList"
-            element = {<EmoplyeeList/>}
+            path="/EmployeeList"
+            element={<EmoplyeeList />}
           />
           <Route
-            path ="/MainPage"
-            element = {<MainPage/>}
+            path="/MainPage"
+            element={<MainPage />}
           />
-      </Routes>
-    </BrowserRouter>
-    </div>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
--- src/components/EmployeeInformation.tsx
+++ src/components/EmployeeInformation.tsx
@@ -1,34 +1,23 @@
+import type { Employee } from "../types/Employee";
+
 interface Props {
-    Name: string;
-    UserName: string;
-    Password: string;
-    Division: string;
-    DocumentType: string;
+    Employees: Employee[]
 }
 
 function EmployeeInformation(
-    { Name, UserName, Password, Division, DocumentType }: Props
+    { Employees }: Props
 ) {
     return (
-        // <table> 
-        //     <thead>
-        //         <tr>
-        //             <th>Name</th>
-        //             <th>User Name</th>
-        //             <th>Division</th>
-        //             <th>Document Type</th>
-        //         </tr>
-        //     </thead>
-        //     <tr>
-
-        //     </tr>
-        // </table>
-        <div>
-            <p>Name : {Name}  </p>
-            <p>UserName : {UserName}  </p>
-            <p>Division : {Division}  </p>
-            <p>Document Type : {DocumentType}  </p>
-        </div>
+        <>
+            {Employees.map((employee) => (
+                <tr key={employee.UserName}>
+                    <td>{employee.Name}</td>
+                    <td>{employee.UserName}</td>
+                    <td>{employee.Division}</td>
+                    <td>{employee.DocumentType}</td>
+                </tr>
+            ))}
+        </>
     );
 }
 
